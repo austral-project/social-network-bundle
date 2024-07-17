699fe28ec9e5aee81e776ee18f1f273510a2e914
Matthieu Beurel <matthieu@mbeurel.com>
2024-07-17 09:23:57 +0200

    feat : Add compatibility Symfony 6.4

--- AustralSocialNetworkBundle.php
+++ AustralSocialNetworkBundle.php
@@ -24,7 +24,7 @@ class AustralSocialNetworkBundle extends Bundle
   /**
    * @param ContainerBuilder $container
    */
-  public function build(ContainerBuilder $container)
+  public function build(ContainerBuilder $container): void
   {
     parent::build($container);
     $container->addCompilerPass(new DoctrineResolveTargetEntityPass(), PassConfig::TYPE_BEFORE_OPTIMIZATION, 1000);
